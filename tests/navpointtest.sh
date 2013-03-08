5df67422e616fdeb48f98778e6c391da694497e7
Johan Abbors <johan@pratsam.com>
2013-03-08 12:21:37 +0200

    Add daisy book with pages

--- tests/navpointtest.sh
+++ tests/navpointtest.sh
@@ -8,3 +8,4 @@ $PREFIX ./navpointtest ${srcdir:-.}/data/Mountains_skip/ncc.html
 $PREFIX ./navpointtest ${srcdir:-.}/data/Theory_behind_players_kate/ncc.html
 $PREFIX ./navpointtest ${srcdir:-.}/data/Chimpanzees_DAISY_3.0/Chimpanzees.opf
 $PREFIX ./navpointtest ${srcdir:-.}/data/VBL20120911/speechgen.opf
+$PREFIX ./navpointtest ${srcdir:-.}/data/FireSafety/ncc.html
